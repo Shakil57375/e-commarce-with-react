c73471cc8689d48c64faf9cfeaf61e92c2740351
Shakil57375 <shakil57375@gmail.com>
2023-03-27 15:51:51 +0600

    length added

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {name, img, seller, price, quantity, ratings} = props.product
+    const {name, img, seller, price, ratings} = props.product
+    console.log(props);
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +13,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} star</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className = 'btn-cart'>Add to Cart</button>
         </div>
     );
 };
