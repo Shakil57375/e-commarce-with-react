2515301ba91f4e6e2ab34c8796894c31e9b2bea3
Shakil57375 <shakil57375@gmail.com>
2023-03-27 16:13:30 +0600

    product length added

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
     const {name, img, seller, price, ratings} = props.product
@@ -13,7 +15,10 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} star</p>
             </div>
-            <button onClick={() => handleAddToCart(props.product)} className = 'btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className = 'btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
